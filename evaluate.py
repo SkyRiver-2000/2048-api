5e2e582efbf2198e0b44928a613d2e5b6e5b7bbd
SkyRiver-2000 <58808235+SkyRiver-2000@users.noreply.github.com>
2020-06-20 21:21:57 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,28 +1,29 @@
 from game2048.game import Game
 from game2048.displays import Display
 
-
 def single_run(size, score_to_win, AgentClass, **kwargs):
     game = Game(size, score_to_win)
-    agent = AgentClass(game, display=Display(), **kwargs)
-    agent.play(verbose=True)
+    agent = AgentClass(game, display = Display(), **kwargs)
+    agent.play(verbose = True, max_iter = 5e3)
     return game.score
-
 
 if __name__ == '__main__':
     GAME_SIZE = 4
     SCORE_TO_WIN = 2048
-    N_TESTS = 50
+    N_TESTS = 10
 
     '''====================
-    Use your own agent here.'''
-    from game2048.agents import ExpectiMaxAgent as TestAgent
+    My own agent here.'''
+    from MyAgent import MyOwnAgent as TestAgent
     '''===================='''
+    
+    # game = Game(GAME_SIZE, SCORE_TO_WIN)
+    # print(type(game.board))
 
     scores = []
     for _ in range(N_TESTS):
         score = single_run(GAME_SIZE, SCORE_TO_WIN,
-                           AgentClass=TestAgent)
+                           AgentClass = TestAgent, load_path = "./mdl_final.pkl")
         scores.append(score)
 
     print("Average scores: @%s times" % N_TESTS, sum(scores) / len(scores))
